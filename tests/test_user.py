f58c71a6bcfe98f85b3c0354beb3ce95030ecee5
Dianoil <abdulkadirova-diana@rambler.ru>
2025-11-04 18:56:44 +0300

    1comm

--- tests/test_user.py
+++ tests/test_user.py
@@ -26,16 +26,35 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'nonexistent@mail.com'})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user = {
+        'name': 'Diana Abulkdaifova',
+        'email': 'a.diana@gmail.com'
+    }
+    response = client.post("/api/v1/user", json=new_user)
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)  # Предполагается, что возвращается ID пользователя
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
-
+    existing_user = {
+        'name': 'Ivan Ivanov',
+        'email': users[0]['email']
+    }
+    response = client.post("/api/v1/user", json=existing_user)
+    assert response.status_code == 409
+    assert response.json() == {"detail": "User with this email already exists"}
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+     '''Удаление пользователя'''
+    response = client.delete("/api/v1/user", params={'email': users[0]['email']})
+    assert response.status_code == 204
+    
+    # Проверяем, что пользователь действительно удален
+    response = client.get("/api/v1/user", params={'email': users[0]['email']})
+    assert response.status_code == 404
